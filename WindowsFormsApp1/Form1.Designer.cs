068b478383380c47144b7590508692109e6b6e08
SayyehBan <sdvp1991david@gmail.com>
2024-01-29 14:08:47 +0330

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richText = new System.Windows.Forms.RichTextBox();
+            this.richTranslate = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(707, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "متن:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ترجمه :";
+            // 
+            // richText
+            // 
+            this.richText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richText.Location = new System.Drawing.Point(431, 12);
+            this.richText.Name = "richText";
+            this.richText.Size = new System.Drawing.Size(276, 96);
+            this.richText.TabIndex = 2;
+            this.richText.Text = "";
+            // 
+            // richTranslate
+            // 
+            this.richTranslate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTranslate.Location = new System.Drawing.Point(116, 12);
+            this.richTranslate.Name = "richTranslate";
+            this.richTranslate.Size = new System.Drawing.Size(276, 96);
+            this.richTranslate.TabIndex = 3;
+            this.richTranslate.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ترجمه";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 169);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTranslate);
+            this.Controls.Add(this.richText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richText;
+        private System.Windows.Forms.RichTextBox richTranslate;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<List<string>> TranslateText(string originalText, string inputLanguage, string outputLanguage)
+        {
+            string url = $"https://translate.google.com/translate_a/single?client=gtx&sl={inputLanguage}&tl={outputLanguage}&dt=t&q={originalText}";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string responseText = reader.ReadToEnd();
+                JArray jsonResponse = JArray.Parse(responseText);
+
+                JArray translationParts = (JArray)jsonResponse[0];
+                List<string> translatedParts = new List<string>();
+
+                foreach (JArray part in translationParts)
+                {
+                    string translatedText = (string)part[0];
+                    translatedParts.Add(translatedText);
+                }
+
+                return translatedParts;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string text = richText.Text;
+            string inputLanguage = "fa";
+            string outputLanguage = "en";
+
+            List<string> translations = await TranslateText(text, inputLanguage, outputLanguage);
+
+            string translatedText = string.Join("\n", translations);
+            richTranslate.Text = translatedText;
+        }
+    }
+}
+
